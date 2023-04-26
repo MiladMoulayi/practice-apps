18980c99b7395cc8fbedd52db9b9e76e2af6bb0f
Milad Moulayi <m.moulayi123@gmail.com>
2023-04-26 10:56:04 -0700

    Created HTTP requests for server

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,29 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
 // 3. Export the models
 // 4. Import the models into any modules that need them
+
+mongoose.connect("mongodb://127.0.0.1:27017/glossary", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log("Connected to DB"))
+  .catch(console.error("Could not connect to DB"))
+
+const EntrySchema = new Schema({
+  word: {
+    type: String,
+    required: true
+  },
+  definition: {
+    type: String,
+    required: true
+  }
+});
+
+const Entry = mongoose.model("Entry", EntrySchema);
+
+module.exports = Entry;
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,19 +1,48 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const Entry = require("./db");
 
 const app = express();
+
+app.use(express.json());
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
- * Other routes here....
- *
- * 
- */
+app.get('/glossary', async (req, res) => {
+  const entries = await Entry.find();
+
+  res.json(entries);
+})
+
+app.post('/glossary/new', (req, res) => {
+  const entry = new Entry({
+    word: req.body.word,
+    definition: req.body.def
+  });
+
+  entry.save();
+
+  res.json(entry);
+});
+
+app.delete('/glossary/delete/:id', async (req, res) => {
+  const result = await Entry.findByIdAndDelete(req.params.id);
+
+  res.json(result);
+});
+
+app.put('/glossary/edit/:id', async (req, res) => {
+  const entry = await Entry.findById(req.params.id);
+
+  entry.word = req.body.word;
+  entry.definition = req.body.def;
+
+  entry.save();
+
+  res.json(entry);
+})
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
