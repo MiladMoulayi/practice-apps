8457bac132db000d30825be0e1411e867435b3a3
Milad Moulayi <m.moulayi123@gmail.com>
2023-04-28 10:02:29 -0700

    Added schema.sql and .env file

---
+++ 2-checkout/server/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE checkout;
+
+USE checkout;
+
+CREATE TABLE responses (
+
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+
+);
+
+/*  Execute this file from the command line by typing:
+ *    mysql -u root < server/schema.sql
+ *  to create the database and the tables.*/
